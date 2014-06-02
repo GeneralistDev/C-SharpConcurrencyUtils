0c09eeb533aebe2b9a6a7e0e7db6390e5781d24e
Daniel Parker <dparker.tech@gmail.com>
2014-06-02 16:36:04 +1000

    NEW: FIFO Semaphore

---
+++ ConcurrencyUtilities/FIFOSemaphore.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConcurrencyUtils
+{
+    /// <summary>
+    ///     First in first out Semaphore. A semaphore that retains order of acquires so as to avoid starvation of threads.
+    /// </summary>
+    public class FIFOSemaphore: Semaphore
+    {
+        private Queue<ConcurrencyUtils.Semaphore> waitingThreadsQueue = new Queue<ConcurrencyUtils.Semaphore>();
+        private readonly Object queueLock = new Object();
+
+        public FIFOSemaphore(UInt64 n = 0): base(n){ }
+
+        /// <summary>
+        ///     The acquiring thread creates a semaphore of it's own and adds it to the queue. 
+        ///     The thread then tries to acquire from that semaphore. Once acquired, the thread can acquire a token.
+        /// </summary>
+        public override void Acquire()
+        {
+            Semaphore threadSemaphore = new Semaphore(0);
+            lock (queueLock) 
+            {
+                waitingThreadsQueue.Enqueue(threadSemaphore);
+            }
+            threadSemaphore.Acquire();
+            base.Acquire();
+        }
+
+        /// <summary>
+        ///     Release n tokens. If there are threads waiting, then dequeue and signal them in order.
+        /// </summary>
+        /// <param name="n">The number of tokens to release.</param>
+        public override void Release(UInt64 n = 1)
+        {
+            base.Release(n);
+            lock (queueLock)
+            {
+                if (waitingThreadsQueue.Count > 0)
+                {
+                    UInt64 queueSize = UInt64.Parse(waitingThreadsQueue.Count.ToString());
+                    if (queueSize <= n)
+                    {
+                        for (UInt64 i = 0; i < n; i++)
+                        {
+                            Semaphore aThreadSemaphore = waitingThreadsQueue.Dequeue();
+                            aThreadSemaphore.Release();
+                        }
+                    }
+                    else
+                    {
+                        for (UInt64 i = 0; i < queueSize; i++)
+                        {
+                            Semaphore aThreadSemaphore = waitingThreadsQueue.Dequeue();
+                            aThreadSemaphore.Release();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
