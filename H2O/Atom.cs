1946ca638666fb4d431895cdc53f6ea3277b0e87
Daniel Parker <dparker.tech@gmail.com>
2014-06-20 19:31:39 +1000

    DOC: Full commenting

--- H2O/Atom.cs
+++ H2O/Atom.cs
@@ -4,6 +4,9 @@ using ConcurrencyUtils;
 
 namespace H2O
 {
+	/// <summary>
+	/// Atom class.
+	/// </summary>
 	public class Atom: ActiveObject
 	{
 		private const int DELAY = 3000;
@@ -11,19 +14,28 @@ namespace H2O
 		private Semaphore atomSemaphore;
 		private Barrier bondBarrier;
 
+		/// <summary>
+		/// Initializes a new instance of the Atom class.
+		/// </summary>
+		/// <param name="atomSemaphore">Atom semaphore.</param>
+		/// <param name="bondBarrier">Bond barrier.</param>
+		/// <param name="name">Name.</param>
 		public Atom(Semaphore atomSemaphore, Barrier bondBarrier, string name) : base(name)
 		{
 			this.atomSemaphore = atomSemaphore;
 			this.bondBarrier = bondBarrier;
 		}
 
+		/// <summary>
+		/// Execute atom logic.
+		/// </summary>
 		protected override void Execute()
 		{
 			while (true)
 			{
-				atomSemaphore.Acquire();
+				atomSemaphore.Acquire();		// Get permission to start bonding process
 				Console.WriteLine(Thread.CurrentThread.Name + " waiting to bond...");
-				if (bondBarrier.Arrive())
+				if (bondBarrier.Arrive())		// Wait for other atoms
 				{
 					Console.WriteLine("\t\tCreating H2O...");
 				}
@@ -36,7 +48,7 @@ namespace H2O
 		{
 			Console.WriteLine("\t\t\t" + Thread.CurrentThread.Name + " bonding...");
 			Thread.Sleep(DELAY);
-			atomSemaphore.Release();
+			atomSemaphore.Release();			// Allow other atoms to start bonding
 		}
 	}
 }
--- H2O/Program.cs
+++ H2O/Program.cs
@@ -4,15 +4,22 @@ using ConcurrencyUtils;
 
 namespace H2O
 {
+	/// <summary>
+	/// 	H2O Program Class.
+	/// </summary>
 	public class Program
 	{
+		/// <summary>
+		/// The entry point of the program, where the program control starts and ends.
+		/// </summary>
+		/// <param name="args">The command-line arguments.</param>
 		public static void Main(string[] args)
 		{
-			Semaphore hydrogenSemaphore = new Semaphore(2);
-			Semaphore oxygenSemaphore = new Semaphore(1);
+			Semaphore hydrogenSemaphore = new Semaphore(2);	// Controls the amount of hydrogen present in the barrier. 
+			Semaphore oxygenSemaphore = new Semaphore(1);	// Controls the amount of oxygen present in the barrier.
 			Barrier bondBarrier = new Barrier(3);
 
-			ArrayList atoms = new ArrayList();
+			/* Create six oxygen and hydrogen atoms */
 			for (int i = 0; i < 6; i++)
 			{
 				Atom hydrogen = new Atom(hydrogenSemaphore, bondBarrier, "H" + i);
