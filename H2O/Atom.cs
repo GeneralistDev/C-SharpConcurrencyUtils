fadcf14bc61a69d5eaea86c6df125bc40391acd3
Daniel Parker <dparker.tech@gmail.com>
2014-06-20 19:20:47 +1000

    NEW: H2O Program

---
+++ H2O/Atom.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+using ConcurrencyUtils;
+
+namespace H2O
+{
+	public class Atom: ActiveObject
+	{
+		private const int DELAY = 3000;
+
+		private Semaphore atomSemaphore;
+		private Barrier bondBarrier;
+
+		public Atom(Semaphore atomSemaphore, Barrier bondBarrier, string name) : base(name)
+		{
+			this.atomSemaphore = atomSemaphore;
+			this.bondBarrier = bondBarrier;
+		}
+
+		protected override void Execute()
+		{
+			while (true)
+			{
+				atomSemaphore.Acquire();
+				Console.WriteLine(Thread.CurrentThread.Name + " waiting to bond...");
+				if (bondBarrier.Arrive())
+				{
+					Console.WriteLine("\t\tCreating H2O...");
+				}
+				Bond();
+				Thread.Sleep(DELAY);
+			}
+		}
+
+		public void Bond()
+		{
+			Console.WriteLine("\t\t\t" + Thread.CurrentThread.Name + " bonding...");
+			Thread.Sleep(DELAY);
+			atomSemaphore.Release();
+		}
+	}
+}
+
---
+++ H2O/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using ConcurrencyUtils;
+
+namespace H2O
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Semaphore hydrogenSemaphore = new Semaphore(2);
+			Semaphore oxygenSemaphore = new Semaphore(1);
+			Barrier bondBarrier = new Barrier(3);
+
+			ArrayList atoms = new ArrayList();
+			for (int i = 0; i < 6; i++)
+			{
+				Atom hydrogen = new Atom(hydrogenSemaphore, bondBarrier, "H" + i);
+				Atom oxygen = new Atom(oxygenSemaphore, bondBarrier, "O" + i);
+				hydrogen.Start();
+				oxygen.Start();
+			}
+		}
+	}
+}
